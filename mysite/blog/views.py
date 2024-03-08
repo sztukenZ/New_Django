552567476f2e5f701d833e5c486a2a6c63d6ceb8
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2024-03-08 19:44:47 +0100

    Initial commit

---
+++ mysite/blog/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+# Create your views here.
+def post_list(request):
+    posts_list = Post.published.all()
+    # Pagination with 3 posts per site
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page number is not an integer, deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day
+                             )
+
+    return render(request, 'blog/post/detail.html', {'post': post})
+
